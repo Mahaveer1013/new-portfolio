2e4c099e165a97a5b13189e2a3a92531c00a1f4c
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2024-06-18 21:49:06 +0530

    project details remaining

--- src/project_data.js
+++ src/project_data.js
@@ -1,9 +1,19 @@
 export const industry_projects = [
     {
-      title: 'Biometric Attendance and Employee Management System',
-      company: 'Kanchi Karpooram Limited',
-      tech: 'Html, Css, Js, Flask and Sqlalchemy',
-      role: 'Backend Developer',
+        title: 'Biometric Attendance and Employee Management System',
+        company: 'Kanchi Karpooram Limited',
+        tech: 'Html, Css, Js, Flask and Sqlalchemy',
+        role: 'Backend Developer',
+        description: `This Project is to achieve the strict constraints of the company for their attendance system.
+I have worked as a backend developer in this project. We, A team of 4 members have been worked on this project.We use Flask for Backend and Html, Css, Js for Frontend. 
+
+1. The project is basically about making a software for calculating the attendance for their employees.
+2. There is already a software provided along with the biometric machine , which runs in their localhost and it has only basic constraint of attendance.
+3. They download the excel sheet of attendance daily and employee modifies the data according to the company's constraint which takes around 1hr/per day...
+4. Our software takes only the punch time and emp_number from that software's database and insert into our database every time a new punch data record added, and calculate attendance according to the constrainsts that company required... and provided many features they asked.
+5. For database , we used SQLAlchemy (toolkit of python SQL) .`,
+        duration: '1 year',
+        screenshots: 'main-page-event.png'
     },
     {
       title: 'Internship',
